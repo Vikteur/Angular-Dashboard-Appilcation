1fa174c410ac700652a1eb2ec56052afd0a84117
Viktor <viktor.vansteenwehgen@ordina.be>
2024-09-27 08:39:02 +0200

    ngOninit and server status variable added

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-server-status',
-  standalone: true,
-  imports: [],
-  templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+    selector: 'app-server-status',
+    standalone: true,
+    imports: [],
+    templateUrl: './server-status.component.html',
+    styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit{
+    currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+    constructor() {}
+
+    ngOnInit() {
+        setInterval(() => {
+            const rnd = Math.random();
+            console.log(rnd);
+            if (rnd < 0.5) {
+                this.currentStatus = 'online';
+            } else if (rnd < 0.9) {
+                this.currentStatus = 'offline';
+            } else {
+                this.currentStatus = 'unknown';
+            }
+        }, 5000);
+    }
+
 }
